cde881786fc536048929366e23e49a55b002ff94
Kiki <Kiki@Kristijan>
2018-10-08 22:47:55 +0200

    Testiranje

---
+++ TestApp/src/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+
+    var myString = "Hello World"
+
+    for (i in 1..3){
+        println(myString)
+    }
+}
+
+class Person(){
+    var ime: String
+    var prezime: String
+}
