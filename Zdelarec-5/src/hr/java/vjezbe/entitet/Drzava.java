7ffe57771d94d1abe928a25b6d6489192745fb23
Kiki <Kiki@Kristijan>
2018-10-08 23:23:22 +0200

    Initial push to GitHub

---
+++ Zdelarec-5/src/hr/java/vjezbe/entitet/Drzava.java
@@ -0,0 +1,111 @@
+package hr.java.vjezbe.entitet;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Predstavlja entitet drzave koji je definiran nazivom i povrsinom.
+ * 
+ * @author Kristijan Zdelarec
+ *
+ */
+public class Drzava {
+
+	private String naziv;
+	private BigDecimal povrsina;
+	private List<Zupanija> popisZupanijaDrzave;
+
+	/**
+	 * Inicijalizira podatak o nazivu drzave i njenoj povrsini.
+	 * 
+	 * @param naziv
+	 *            podatak o nazivu drzave
+	 * @param povrsina
+	 *            podatak o povrsini drzave
+	 */
+	public Drzava(String naziv, BigDecimal povrsina) {
+		this.naziv = naziv;
+		this.povrsina = povrsina;
+		popisZupanijaDrzave = new ArrayList<>();
+	}
+
+	/**
+	 * Vraca naziv entiteta Drzava.
+	 * 
+	 * @return vraca podatak o nazivu drzave
+	 */
+	public String getNaziv() {
+		return naziv;
+	}
+
+	/**
+	 * Postavlja naziv drzave.
+	 * 
+	 * @param naziv
+	 *            podatak o nazivu drzave
+	 */
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	/**
+	 * Vraca povrsinu entiteta Drzava.
+	 * 
+	 * @return vraca podatak o povrsini drzave
+	 */
+	public BigDecimal getPovrsina() {
+		return povrsina;
+	}
+
+	/**
+	 * Postavlja vrijednost za povrsinu drzave.
+	 * 
+	 * @param povrsina
+	 *            podatak o povrsini drzave
+	 */
+	public void setPovrsina(BigDecimal povrsina) {
+		this.povrsina = povrsina;
+	}
+
+
+	/**
+	 * Dohvaca popis zupanija  neke drzave.
+	 * 
+	 * @return vraca popis zupanije neke drzave
+	 */
+	public List<Zupanija> getPopisZupanijaDrzave() {
+		return popisZupanijaDrzave;
+	}
+
+	/**
+	 * Postavlja vrijednosti liste zupanija.
+	 * 
+	 * @param popisZupanijaDrzave popis zupanija u nekoj drzavi
+	 */
+	public void setPopisZupanijaDrzave(List<Zupanija> popisZupanijaDrzave) {
+		this.popisZupanijaDrzave = popisZupanijaDrzave;
+	}
+
+	/**
+	 *  Dohvaca popis zupanija  neke drzave.
+	 * 
+	 * @return vraca popis zupanije neke drzave
+	 */
+	public List<Zupanija> getZupanije() {
+		return popisZupanijaDrzave;
+	}
+
+	/**
+	 * Dohvaca objekt klase Drzava.
+	 * 
+	 * @return vraca objekt klase drzava
+	 */
+	public Drzava getDrzava() {
+		return this;
+	}
+	
+
+	
+
+}
---
+++ Zdelarec-5/src/hr/java/vjezbe/entitet/Senzor.java
@@ -0,0 +1,108 @@
+package hr.java.vjezbe.entitet;
+
+import java.math.BigDecimal;
+
+/**
+ * Predstavlja zajednicka svojstva svih senzora koji nastanu nasljedivanjem ove klase.
+ * 
+ * @author Kristijan Zdelarec
+ *
+ */
+public abstract class Senzor {
+
+	private String mjernaJedinica;
+	private byte preciznost;
+	private BigDecimal vrijednost;
+	private RadSenzora radSenzora;
+	/**
+	 * Inicijalizira podatke o jedinici mjerenja i preciznosti mjerenja senzora.
+	 * 
+	 * @param mjernaJedinica mjerna jedinica senzora
+	 * @param preciznost prosjecno odstupanje senzora
+	 */
+	protected Senzor(String mjernaJedinica, byte preciznost) {
+		super();
+		this.mjernaJedinica = mjernaJedinica;
+		this.preciznost = preciznost;
+	}
+	
+	/**
+	 * Dohvaca vrijednosti senzora.
+	 * 
+	 * @return vraca podatke o vrijednostima senzora
+	 */
+	abstract String dohvatiVrijednost();
+
+	/**
+	 * Dohvaca mjernu jedinicu senzora.
+	 * 
+	 * @return vraca podatak o mjernoj jedinici senzora
+	 */
+	public String getMjernaJedinica() {
+		return mjernaJedinica;
+	}
+
+	/**
+	 * Postavlja podatak o mjernoj jedinici senzora.
+	 * 
+	 * @param mjernaJedinica podatak o mjernoj jedinici senzora
+	 */
+	public void setMjernaJedinica(String mjernaJedinica) {
+		this.mjernaJedinica = mjernaJedinica;
+	}
+
+	/**
+	 * Dohvac podatak o prosjecnom odstupanju senzora.
+	 * 
+	 * @return vraca podaak o prosjecnom odstupanju senzora
+	 */
+	public byte getPreciznost() {
+		return preciznost;
+	}
+
+	/**
+	 * Postavlja vrijednos preciznosti senzora.
+	 * 
+	 * @param preciznost podatak o prosjecnom odstupanju senzora
+	 */
+	public void setPreciznost(byte preciznost) {
+		this.preciznost = preciznost;
+	}
+
+	/**
+	 * Dohvaca vrijednost mjerenja.
+	 * 
+	 * @return vraca izmjereni podatak
+	 */
+	public BigDecimal getVrijednost() {
+		return vrijednost;
+	}
+
+	/**
+	 * Postavlja vrijednost mjerenja senzora.
+	 * 
+	 * @param vrijednost izmjerena vrijednost senzora
+	 */
+	public void setVrijednost(BigDecimal vrijednost) {
+		this.vrijednost = vrijednost;
+	}
+
+	/**
+	 * Dohvaca podatak o nacinu rada senzora.
+	 * 
+	 * @return vraca podatak o vrsti mjesta
+	 */
+	public RadSenzora getRadSenzora() {
+		return radSenzora;
+	}
+
+	/**
+	 * Postavlja podatak o vrsti mjesta.
+	 * 
+	 * @param radSenzora podatak o nacinu rada senzora
+	 */
+	public void setRadSenzora(RadSenzora radSenzora) {
+		this.radSenzora = radSenzora;
+	}
+
+}
